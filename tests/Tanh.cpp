efc244cb6869ce49a6b651c28fcfb83f1095c1b2
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-05 00:26:17 +0100

    Tanh activation function

---
+++ tests/Tanh.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "brainy/neural/activation/Tanh.hpp"
+
+TEST(Tanh, Function)
+{
+  brainy::neural::Tanh tanh;
+  EXPECT_NEAR(-0.76159, tanh.primary(-1.00000), 0.00001);
+  EXPECT_NEAR( 0.00000, tanh.primary( 0.00000), 0.00001);
+  EXPECT_NEAR( 0.76159, tanh.primary( 1.00000), 0.00001);
+};
+
+TEST(Tanh, Derivative)
+{
+  brainy::neural::Tanh tanh;
+  EXPECT_NEAR(0.41998, tanh.derivative(-0.76159), 0.00001);
+  EXPECT_NEAR(1.00000, tanh.derivative( 0.00000), 0.00001);
+  EXPECT_NEAR(0.41998, tanh.derivative( 0.76159), 0.00001);
+};
