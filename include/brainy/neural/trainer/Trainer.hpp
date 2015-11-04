9da0bef1c121ecaa5db5155b6b7dcffff7371851
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-05 00:29:12 +0100

    Trainer class

---
+++ include/brainy/neural/trainer/Trainer.hpp
@@ -0,0 +1,67 @@
+/*
+ *
+ * Copyright 2015 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BRAINY_NEURAL_TRAINER_TRAINER_HPP
+#define BRAINY_NEURAL_TRAINER_TRAINER_HPP
+
+#include <vector>
+#include "brainy/neural/TrainingPair.hpp"
+#include "brainy/neural/error/MSE.hpp"
+
+namespace brainy {
+  namespace neural {
+    class Network;
+    class Plugin;
+
+    class Trainer {
+    public:
+      Trainer(Network& network) : network(network), error(*(new MSE())) {}
+      void addPlugin(Plugin &plugin);
+      virtual void preTrain();
+      virtual void preEpoch();
+      virtual void postEpoch();
+      virtual void postTrain();
+      virtual void train();
+      virtual void applyBatchCorrections() {};
+      virtual void epoch() = 0;
+      virtual void setGoal(const double goal);
+      virtual void addTrainingPair(TrainingPair &pair);
+      virtual void appendTrainingPairs(std::vector<TrainingPair*> &pairs);
+      virtual void setLearningRate(const double learningRate);
+      virtual double getLearningRate();
+      virtual double getPrevEpochError();
+      virtual std::vector<TrainingPair*> &getTrainingSet();
+      double getMomentum();
+      void setMomentum(const double momentum);
+      int getIteration();
+
+    protected:
+      Network& network;
+      ErrorFunction& error;
+      std::vector<TrainingPair*> trainingSet;
+      std::vector<Plugin*> plugins;
+      int maxEpochs = 100000000;
+      int iteration;
+      double goal = 0.1;
+      double momentum = 0.3;
+      double learningRate = 0.7;
+      double prevEpochError;
+    };
+  }
+}
+
+#endif
---
+++ src/neural/trainer/Trainer.cpp
@@ -0,0 +1,120 @@
+/*
+ *
+ * Copyright 2015 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "brainy/neural/trainer/Trainer.hpp"
+#include "brainy/neural/trainer/plugin/Plugin.hpp"
+
+#include <iostream>
+
+namespace brainy {
+  namespace neural {
+    void Trainer::preTrain() {
+      iteration = 0;
+
+      for (auto plugin : plugins) {
+        plugin->preTrain();
+      }
+    }
+
+    void Trainer::preEpoch() {
+      iteration++;
+      prevEpochError = error.getResult();
+      error.reset();
+
+      for (auto plugin : plugins) {
+        plugin->preEpoch();
+      }
+    }
+
+    void Trainer::postEpoch() {
+      applyBatchCorrections();
+
+      for (auto plugin : plugins) {
+        plugin->postEpoch();
+      }
+    }
+
+    void Trainer::postTrain() {
+      for (auto plugin : plugins) {
+        plugin->postTrain();
+      }
+    }
+
+    void Trainer::train() {
+      preTrain();
+
+      while (true) {
+        preEpoch();
+        epoch();
+        postEpoch();
+
+        if (prevEpochError <= goal || iteration >= maxEpochs) {
+          break;
+        }
+      }
+
+      postTrain();
+    }
+
+    void Trainer::setGoal(const double goal) {
+      this->goal = goal;
+    }
+
+    void Trainer::addTrainingPair(TrainingPair &pair) {
+      trainingSet.push_back(&pair);
+    }
+
+    void Trainer::appendTrainingPairs(std::vector<TrainingPair*> &pairs) {
+      for (auto item : pairs) {
+        addTrainingPair(*item);
+      }
+    }
+
+    void Trainer::setLearningRate(const double learningRate) {
+      this->learningRate = learningRate;
+    }
+
+    std::vector<TrainingPair*> &Trainer::getTrainingSet() {
+      return trainingSet;
+    }
+
+    double Trainer::getLearningRate() {
+      return learningRate;
+    }
+
+    double Trainer::getPrevEpochError() {
+      return prevEpochError;
+    }
+
+    void Trainer::addPlugin(Plugin &plugin) {
+      plugins.push_back(&plugin);
+      plugin.setTrainer(*this);
+    }
+
+    int Trainer::getIteration() {
+      return iteration;
+    }
+
+    double Trainer::getMomentum() {
+      return momentum;
+    }
+
+    void Trainer::setMomentum(const double momentum) {
+      this->momentum = momentum;
+    }
+  }
+}
