953a9affc441162c0ca2765c8bc702f620938007
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-05 00:27:16 +0100

    RMS error function

---
+++ include/brainy/neural/error/RMS.hpp
@@ -0,0 +1,34 @@
+/*
+ *
+ * Copyright 2015 Krzysztof Magosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BRAINY_NEURAL_ERROR_RMS_HPP
+#define BRAINY_NEURAL_ERROR_RMS_HPP
+
+#include <vector>
+#include "brainy/neural/error/ErrorFunction.hpp"
+
+namespace brainy {
+  namespace neural {
+    class RMS : public ErrorFunction {
+    public:
+      virtual std::vector<double> calculate(const std::vector<double> actual, const std::vector<double> expected);
+      virtual double getResult();
+    };
+  }
+}
+
+#endif
