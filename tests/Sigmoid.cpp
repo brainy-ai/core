b51750254c577394bc5c6aa118880385bc001628
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-05 00:26:35 +0100

    Sigmoid activation function

---
+++ tests/Sigmoid.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "brainy/neural/activation/Sigmoid.hpp"
+
+TEST(SigmoidTest, Function)
+{
+  brainy::neural::Sigmoid sigmoid;
+  EXPECT_NEAR(0.50000, sigmoid.primary(0.00000), 0.00001);
+  EXPECT_NEAR(0.62245, sigmoid.primary(0.50000), 0.00001);
+  EXPECT_NEAR(0.73105, sigmoid.primary(1.00000), 0.00001);
+};
+
+TEST(Sigmoid, Derivative)
+{
+  brainy::neural::Sigmoid sigmoid;
+  EXPECT_NEAR(0.25000, sigmoid.derivative(0.50000), 0.00001);
+  EXPECT_NEAR(0.23500, sigmoid.derivative(0.62245), 0.00001);
+  EXPECT_NEAR(0.19661, sigmoid.derivative(0.73105), 0.00001);
+};
