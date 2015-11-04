668c239e5694be2b1af52305188f1c87a5e8d52a
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-05 00:24:47 +0100

    +ext

---
+++ ext/brainy/lib/brainy.rb
@@ -0,0 +1,8 @@
+require "brainy/version"
+
+module Brainy
+  # Your code goes here...
+end
+
+require File.expand_path('../termbox/brainy', __FILE__)
+require File.expand_path('../termbox/brainy', __FILE__)
---
+++ ext/brainy/lib/brainy/version.rb
@@ -0,0 +1,3 @@
+module Brainy
+  VERSION = "0.1.0"
+end
