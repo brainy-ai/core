89b67f84460717f1fb71f9c0aaf84618e90eb634
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-07 17:02:30 +0100

    +monitor

--- include/brainy/neural/trainer/plugin/Monitor.hpp
+++ include/brainy/neural/trainer/plugin/Monitor.hpp
@@ -19,17 +19,22 @@
 #define BRAINY_NEURAL_TRAINER_PLUGIN_MONITOR_HPP
 
 #include "brainy/neural/trainer/plugin/Plugin.hpp"
+#include "brainy/neural/trainer/LearningRate.hpp"
+#include "brainy/neural/trainer/Momentum.hpp"
 
 namespace brainy {
   namespace neural {
     class Monitor : public Plugin {
     public:
+      void init();
       void preTrain();
       void postEpoch();
       void postTrain();
 
     private:
       void show();
+      LearningRate *learningRate;
+      Momentum *momentum;
 
       int cycles = 10000;
       int lastShow;
