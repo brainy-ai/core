7e15a28f4d1c6e309262701a83abcd2eee410c58
Krzysztof Magosa <krzysztof@magosa.pl>
2015-11-25 22:34:05 +0100

    xxx

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . (
-         (company-c-headers-path-user . ("/Users/hexio/projects/cpp/brainy/src"))
-         (flycheck-clang-include-path . ("/Users/hexio/projects/cpp/brainy/src"))
-         )))
